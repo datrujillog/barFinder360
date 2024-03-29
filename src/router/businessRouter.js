9b6d8f9ee459bb5eadb985e7481bbe6daf6bcf97
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-29 18:11:11 -0500

    Fix import statement in businessRouter.js

--- src/router/businessRouter.js
+++ src/router/businessRouter.js
@@ -2,7 +2,7 @@ import express, { response } from "express";
 
 import BusinessService from "../service/businessService.js";
 
-// import { errorResponse, authResponse, Responsee } from "../helpers/response.js";
+import { errorResponse, authResponse, Responsee } from "../helper/response.js";
 // import { valitorUserSignup } from "../middleware/express-validator.js";
 
 function businessRouter(app) {
@@ -17,9 +17,30 @@ function businessRouter(app) {
         try {
             // const data = req.body;
             const response = await businessServ.getBusinesses();
-            response.success ? res.status(201).json({response}) : res.status(400).json({error:response.error.message});
+            response.success ? res.status(201).json({ response }) : res.status(400).json({ error: response.error.message });
         } catch (error) {
             res.status(400).json({ message: error.message });
+        }
+    });
+
+    router.get("/one/:id", async (req, res) => {
+        try {
+            const businessId = req.params.id;
+            const response = await businessServ.businessById(businessId);
+
+            if (response.success) {
+                res.json({
+                    ok: true,
+                    business: response.business
+                });
+
+            }
+
+
+
+        } catch (error) {
+            // res.status(400).json({ message: error.message });
+            errorResponse(res, error.message)
         }
     });
 
