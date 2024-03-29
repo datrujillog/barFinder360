06a097009dd8a427327deadc86fd1e5f0399c1ae
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-29 08:34:19 -0500

    Add urlMongodb to config.js

--- src/config/config.js
+++ src/config/config.js
@@ -7,6 +7,7 @@ import 'dotenv/config'
  const config = {
     jwtSecret: process.env.JW_SECRET || 'secret',
     port: process.env.PORT || 5000,
+    urlMongodb : process.env.DATABASE_URL
  };
 
  export default config;
