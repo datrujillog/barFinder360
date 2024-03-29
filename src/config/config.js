5bed8c2d94001d7bb7325f9d2c0682e0b9861e8c
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-28 23:17:17 -0500

    Update port number in config.js

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ import 'dotenv/config'
 
  const config = {
     jwtSecret: process.env.JW_SECRET || 'secret',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
  };
 
  export default config;
