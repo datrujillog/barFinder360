764d9225ad64849dac7e0a993ce4ebca539129a8
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-29 20:37:54 -0500

    Add authentication check in role creation endpoint

--- src/router/roleRouter.js
+++ src/router/roleRouter.js
@@ -5,6 +5,8 @@ import RoleService from "../service/roleService.js";
 
 
 import { authResponse, errorResponse, Responsee } from "../helper/response.js";
+import { extractDataFromToken } from "../helper/auth.js";
+import { BadRequest } from "../middleware/errors.js";
 
 // import { valitorUserSignup } from "../middleware/express-validator.js";
 
@@ -17,8 +19,16 @@ function businessRouter(app) {
     app.use("/api/role", router);
 
     router.post("/create", async (req, res) => {
-
+        const businessId  = req.headers.businessid; 
         const token = req.cookies.token;
+        const dataToken = await extractDataFromToken(token)
+        try {
+            if(dataToken.id !== businessId) throw new BadRequest('Error de autenticacion')            
+        } catch (error) {
+            return errorResponse(res, error.message)
+            
+        }
+
         const response = await roleServ.createRole(req.body, token);
         response.success
             ? Responsee(res, 201, true, "Rol creado correctamente", { user: response.user })
