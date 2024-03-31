8c4bd7dac4d50ad2f873dce4c3f5aedc74ca67dd
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-31 09:29:57 -0500

    Add categoryCreate method to CategoryService

--- src/service/categoryService.js
+++ src/service/categoryService.js
@@ -9,7 +9,40 @@ class CategoryService {
         console.log('Category Service is created');
     }
 
-    
+    async categoryCreate(businessId, body) {
+        try {
+            const category = {
+                businessId: new ObjectId(businessId),
+                name: body.name,
+                description: body.description,
+                asset: body.asset,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+            console.log(category)
+
+            const table = await db.collection('bar_categories').insertMany([category]);
+            if (table.insertedCount === 0) throw new Error('Category not created');
+
+            console.log(table)
+
+            const insertedIds = table.insertedIds;
+            const insertedData = Object.keys(insertedIds).map(key => ({
+                _id: insertedIds[key],
+                ...category
+            }));
+
+            return {
+                success: true,
+                category:insertedData
+                
+            };
+        } catch (error) {
+            return { success: false, error };
+        }
+    }
+
+
 }
 
-module.exports = CategoryService;
+export default CategoryService;
