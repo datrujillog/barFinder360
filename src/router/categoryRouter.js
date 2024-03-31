0ce43e0524db3395af75f073b4f6c83490ce04bf
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-31 08:40:07 -0500

    Add categoryRouter module

---
+++ src/router/categoryRouter.js
@@ -0,0 +1,24 @@
+
+import express, { response } from "express";
+
+
+import { BadRequest } from "../middleware/errors.js";
+
+import { authResponse, errorResponse, Responsee } from "../helper/response.js";
+import { extractDataFromToken } from "../helper/auth.js";
+import OrderService from "../service/orderService.js";
+
+
+
+function categoryRouter(app) {
+    const router = express.Router();
+
+    //instanciar el servicio
+    const roleServ = new OrderService();
+
+    app.use("/api/v1/category", router);
+
+
+}
+
+export default categoryRouter;
