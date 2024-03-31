9cd1d42239a178aece260b229d299c8bad6551b7
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-31 08:44:57 -0500

    Add CategoryService class

---
+++ src/service/categoryService.js
@@ -0,0 +1,15 @@
+import { ObjectId } from "bson";
+
+import { BadRequest } from "../middleware/errors.js";
+
+import db from "../database/db.js";
+
+class CategoryService {
+    constructor() {
+        console.log('Category Service is created');
+    }
+
+    
+}
+
+module.exports = CategoryService;
