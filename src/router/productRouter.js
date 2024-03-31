80670a82c5093f1a09cdd8938db2b307fd585253
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-31 11:42:16 -0500

    Add product creation route to productRouter.js

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -17,6 +17,27 @@ function productRouter(app) {
 
     app.use("/api/v1/product", router);
 
+    router.post("/create", async (req, res, next) => {
+        try {
+            const businessId = req.headers.businessid;
+            const body = req.body;
+            const token = req.cookies.token;
+            const result = await validateBusiness(businessId, token);
+            if(!result.success) throw new BadRequest(result.error.message);
+            
+            const { success, category } = await roleServ.productCreate(businessId, body);
+            success
+                ? authResponse(res, 201, true, "User created", {
+                    payload: category,
+                    token: token,
+                })
+                : errorResponse(res, response.error);
+
+        } catch (error) {
+            errorResponse(res, error.message);
+        }
+    });
+
 
 }
 
