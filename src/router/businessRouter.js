0288e8e403fa4288b26be5e9375b9c16bfc7180a
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-30 19:43:11 -0500

    Update businessRouter.js to include API endpoint for listing users by business

--- src/router/businessRouter.js
+++ src/router/businessRouter.js
@@ -3,6 +3,8 @@ import express, { response } from "express";
 import BusinessService from "../service/businessService.js";
 
 import { errorResponse, authResponse, Responsee } from "../helper/response.js";
+import { extractDataFromToken } from "../helper/auth.js";
+import { BadRequest } from "../middleware/errors.js";
 // import { valitorUserSignup } from "../middleware/express-validator.js";
 
 function businessRouter(app) {
@@ -11,7 +13,7 @@ function businessRouter(app) {
     //instanciar el servicio
     const businessServ = new BusinessService();
 
-    app.use("/api/business", router);
+    app.use("/api/v1/business", router);
 
     router.get("/", async (req, res) => {
         try {
@@ -40,6 +42,30 @@ function businessRouter(app) {
 
         } catch (error) {
             // res.status(400).json({ message: error.message });
+            errorResponse(res, error.message)
+        }
+    });
+
+    //listar todosd los usuarios que pertenescan a un negocio
+    router.get("/users/:id", async (req, res) => {
+        try {
+            const businessId = req.headers.businessid;
+            const idBusiness = req.params.id;
+            const token = req.cookies.token;
+            const dataToken = await extractDataFromToken(token)
+
+            if (dataToken.businessId !== businessId) throw new BadRequest('Error de autenticacion')
+
+            const response = await businessServ.usersByBusiness(businessId, idBusiness);
+
+            if (response.success) {
+                res.json({
+                    ok: true,
+                    users: response.users
+                });
+
+            }
+        } catch (error) {
             errorResponse(res, error.message)
         }
     });
