769192c70b3a353f5bd828a20dd781fe3db0028b
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-31 09:29:45 -0500

    Add category creation endpoint to categoryRouter.js

--- src/router/categoryRouter.js
+++ src/router/categoryRouter.js
@@ -1,6 +1,7 @@
 
 import express, { response } from "express";
 
+import CategoryService from "../service/categoryService.js";
 
 import { BadRequest } from "../middleware/errors.js";
 
@@ -14,12 +15,33 @@ function categoryRouter(app) {
     const router = express.Router();
 
     //instanciar el servicio
-    const roleServ = new OrderService();
+    const categoryServ = new CategoryService();
 
     app.use("/api/v1/category", router);
+    // router.post["get"], "/create", async (req, res, next) => {4
+
+    router.post("/create", async (req, res) => {
+        const businessId = req.headers.businessid;
+        const body = req.body;
+        const token = req.cookies.token;
+        const dataToken = await extractDataFromToken(token)
+        try {
+            if (dataToken.businessId !== businessId) throw new BadRequest('Error de autenticacion')
+        } catch (error) {
+            return errorResponse(res, error.message)
+
+        }
+        const { success, category } = await categoryServ.categoryCreate(businessId, body);
+        success
+            ? authResponse(res, 201, true, "User created", {
+                payload: category,
+                token: token,
+            })
+            : errorResponse(res, response.error);
+    });
 
 
-    
+
 
 
 }
