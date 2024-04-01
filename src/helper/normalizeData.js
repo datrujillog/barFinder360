32f190773b42016fe1ebfd8a52783c968d299fb8
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-31 19:28:09 -0500

    Add parseProduct function to normalizeData.js

--- src/helper/normalizeData.js
+++ src/helper/normalizeData.js
@@ -1,6 +1,7 @@
 
 import { ObjectId } from 'mongodb';
 import log4js from 'log4js'
+import { BadRequest } from '../middleware/errors.js';
 
 const log = log4js.getLogger(`${process.env.NODE_ENV} - normalizeData`);
 
@@ -31,7 +32,7 @@ let parseNewRols = async (obj, objAuthorizationPlantilla) => {
 
 
 let valuesNestedRecusive = async (dataBody, dataNormalice) => {
-    const object = {} 
+    const object = {}
     let i = 1;
     for (const property in dataNormalice) {
 
@@ -56,6 +57,50 @@ let valuesNestedRecusive = async (dataBody, dataNormalice) => {
     return object
 }
 
+
+
+const parseProduct = async (body, businessId) => {
+    const requiredFields = ['name', 'type', 'code', 'salePrice', 'TimePreparation', 'image', 'categoryId'];
+
+    try {
+        // Verificar si todos los campos obligatorios están presentes
+        for (const field of requiredFields) {
+            if (!(field in body)) {
+                throw new BadRequest(`Falta el campo obligatorio: ${field}`);
+            }
+        }
+
+        // Definir los valores por defecto para los campos opcionales
+        const asset = [false, true];
+        const status = ['created', 'deleted', 'updated'];
+
+        // Crear el objeto product
+        const product = {
+            name: body.name,
+            type: body.type,
+            code: body.code,
+            salePrice: body.salePrice,
+            TimePreparation: body.TimePreparation,
+            description: body.description || '',
+            asset: asset[body.asset],
+            image: body.image,
+            status: status[body.status] || 'created',
+            categoryId: new ObjectId(body.categoryId),
+            promotionId: body.promotionId ? new ObjectId(body.promotionId) : null,
+            businessId: new ObjectId(businessId),
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+
+        return product;
+    } catch (error) {
+        // Devolver un objeto con éxito false y el mensaje de error
+        return { success: false, error: error.message };
+    }
+};
+
+
 export {
-    parseNewRols
+    parseNewRols,
+    parseProduct
 }
