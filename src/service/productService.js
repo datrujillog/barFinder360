ec348952f19ceda27b054a8a7f09850f33737a7e
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-31 20:31:50 -0500

    Refactor ProductService methods

--- src/service/productService.js
+++ src/service/productService.js
@@ -11,14 +11,14 @@ class ProductService {
     }
 
     async productCreate(businessId, product) {
-        try {  
-            const save = await parseProduct(product,businessId)
-            if(save.error) throw new BadRequest(save.error);
+        try {
+            const save = await parseProduct(product, businessId)
+            if (save.error) throw new BadRequest(save.error);
 
             const results = await db.collection('bar_products').insertMany([save]);
             if (results.insertedCount === 0) throw new Error('Category not created');
 
-            const insertedIds =  results.insertedIds
+            const insertedIds = results.insertedIds
             const insertedData = Object.keys(insertedIds).map(key => ({
                 _id: insertedIds[key],
                 ...product
@@ -38,35 +38,35 @@ class ProductService {
         try {
             const results = await db.collection('bar_products').aggregate([
                 {
-                  $match: {
-                    businessId: new ObjectId(businessId),
-                  },
+                    $match: {
+                        businessId: new ObjectId(businessId),
+                    },
                 },
                 {
-                  $lookup: {
-                    from: "bar_categories",
-                    localField: "categoryId",
-                    foreignField: "_id",
-                    as: "category",
-                  },
+                    $lookup: {
+                        from: "bar_categories",
+                        localField: "categoryId",
+                        foreignField: "_id",
+                        as: "category",
+                    },
                 },
                 {
-                  $lookup: {
-                    from: "bar_business",
-                    localField: "businessId",
-                    foreignField: "_id",
-                    as: "business",
-                  },
+                    $lookup: {
+                        from: "bar_business",
+                        localField: "businessId",
+                        foreignField: "_id",
+                        as: "business",
+                    },
                 },
                 {
-                  $lookup: {
-                    from: "bar_promotions",
-                    localField: "promotionsId",
-                    foreignField: "_id",
-                    as: "promotions",
-                  },
+                    $lookup: {
+                        from: "bar_promotions",
+                        localField: "promotionsId",
+                        foreignField: "_id",
+                        as: "promotions",
+                    },
                 },
-              ]).toArray()     
+            ]).toArray()
 
             if (results.length === 0) throw new Error('Products not found');
 
@@ -81,7 +81,38 @@ class ProductService {
 
     async productById(businessId, productId) {
         try {
-            const results = await db.collection('bar_products').findOne({ _id: new ObjectId(productId), businessId: new ObjectId(businessId) });
+            const results = await db.collection('bar_products').aggregate([
+                {
+                    $match: {
+                        _id: new ObjectId(productId),
+                        businessId: new ObjectId(businessId),
+                    },
+                },
+                {
+                    $lookup: {
+                        from: "bar_categories",
+                        localField: "categoryId",
+                        foreignField: "_id",
+                        as: "category",
+                    },
+                },
+                {
+                    $lookup: {
+                        from: "bar_business",
+                        localField: "businessId",
+                        foreignField: "_id",
+                        as: "business",
+                    },
+                },
+                {
+                    $lookup: {
+                        from: "bar_promotions",
+                        localField: "promotionsId",
+                        foreignField: "_id",
+                        as: "promotions",
+                    },
+                },
+            ]).toArray()
             if (!results) throw new Error('Product not found');
 
             return {
@@ -95,8 +126,8 @@ class ProductService {
 
     async productUpdate(businessId, productId, product) {
         try {
-            const save = await parseProduct(product,businessId)
-            if(save.error) throw new BadRequest(save.error);
+            const save = await parseProduct(product, businessId)
+            if (save.error) throw new BadRequest(save.error);
 
             const results = await db.collection('bar_products').updateOne({ _id: new ObjectId(productId), businessId: new ObjectId(businessId) }, { $set: save });
             if (results.modifiedCount === 0) throw new Error('Product not updated');
@@ -123,7 +154,7 @@ class ProductService {
         }
     }
 
-    
+
 }
 
 export default ProductService;
