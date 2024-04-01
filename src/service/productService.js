db5a32fe0e834531b8c2617f4dcc2364d2b75e29
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-03-31 19:56:54 -0500

    Refactor ProductService methods and add new functionality

--- src/service/productService.js
+++ src/service/productService.js
@@ -13,7 +13,7 @@ class ProductService {
     async productCreate(businessId, product) {
         try {  
             const save = await parseProduct(product,businessId)
-            if(!save.success) throw new BadRequest(save.error);
+            if(save.error) throw new BadRequest(save.error);
 
             const results = await db.collection('bar_products').insertMany([save]);
             if (results.insertedCount === 0) throw new Error('Category not created');
@@ -28,6 +28,64 @@ class ProductService {
                 success: true,
                 Product: insertedData
 
+            };
+        } catch (error) {
+            return { success: false, error };
+        }
+    }
+
+    async productList(businessId) {
+        try {
+            const results = await db.collection('bar_products').find({ businessId: new ObjectId(businessId) }).toArray();
+            if (results.length === 0) throw new Error('Products not found');
+
+            return {
+                success: true,
+                Products: results
+            };
+        } catch (error) {
+            return { success: false, error };
+        }
+    }
+
+    async productById(businessId, productId) {
+        try {
+            const results = await db.collection('bar_products').findOne({ _id: new ObjectId(productId), businessId: new ObjectId(businessId) });
+            if (!results) throw new Error('Product not found');
+
+            return {
+                success: true,
+                Product: results
+            };
+        } catch (error) {
+            return { success: false, error };
+        }
+    }
+
+    async productUpdate(businessId, productId, product) {
+        try {
+            const save = await parseProduct(product,businessId)
+            if(save.error) throw new BadRequest(save.error);
+
+            const results = await db.collection('bar_products').updateOne({ _id: new ObjectId(productId), businessId: new ObjectId(businessId) }, { $set: save });
+            if (results.modifiedCount === 0) throw new Error('Product not updated');
+
+            return {
+                success: true,
+                Product: save
+            };
+        } catch (error) {
+            return { success: false, error };
+        }
+    }
+
+    async productDelete(businessId, productId) {
+        try {
+            const results = await db.collection('bar_products').deleteOne({ _id: new ObjectId(productId), businessId: new ObjectId(businessId) });
+            if (results.deletedCount === 0) throw new Error('Product not deleted');
+
+            return {
+                success: true
             };
         } catch (error) {
             return { success: false, error };
