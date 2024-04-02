e9fc6e04afc4c90e537b116fe75cd31f3231959e
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-02 12:00:14 -0500

    Refactor restaurantRepository.js to use OrderModel and add orderUpdate method

--- src/repositories/restaurantRepository.js
+++ src/repositories/restaurantRepository.js
@@ -1,15 +1,18 @@
 
-import RestaurantModel from "../database/db.js";
+import { ObjectId } from 'mongodb';
+import { BadRequest } from "../middleware/errors.js";
+import OrderModel from "../database/db.js";
+
 
 
 class OrderRepository {
     constructor() {
-        this.restaurantModel = RestaurantModel;
+        this.orderModel = OrderModel;
     }
 
     async createOrder(data) {
         try {
-            const results = await this.restaurantModel.collection("bar_orders").insertMany([data]);
+            const results = await this.orderModel.collection("bar_orders").insertMany([data]);
             if (results.acknowledged === false) throw new BadRequest("Error al insertar el pedido");
 
             const insertedIds = results.insertedIds;
@@ -22,6 +25,34 @@ class OrderRepository {
                 success: true,
                 order: insertedData
             }
+        } catch (error) {
+            return { success: false, error };
+        }
+    }
+
+    async orderUpdate(businessId, orderId,save) {
+        try {
+            const results = await this.orderModel.collection('bar_orders').updateOne(
+                {
+                    businessId: new ObjectId(businessId),
+                    _id: new ObjectId(orderId)
+                },
+                {
+                    $set: {
+                        ...save
+                    }
+                }
+            )
+
+            if (results.modifiedCount === 0) {
+                throw new BadRequest('No Ordenes para actualizar');
+            }
+            if(results.acknowledged === false) throw new BadRequest("Error al actualizar el pedido");
+
+            return {
+                success: true,
+                order: results
+            };
         } catch (error) {
             return { success: false, error };
         }
