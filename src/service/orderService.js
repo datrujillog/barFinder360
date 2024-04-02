6ab7e658f0009eb928c2db68814bdc30499b068e
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-01 21:53:23 -0500

    Add userId parameter to orderList method and implement orderById method

--- src/service/orderService.js
+++ src/service/orderService.js
@@ -60,12 +60,67 @@ class OrderService {
     }
   }
 
-  async orderList(businessId) {
+  async orderList(businessId,userId) {
     try {
       const results = await db.collection('bar_orders').aggregate([
         {
           $match: {
             businessId: new ObjectId(businessId),
+          },
+        },
+        {
+          $lookup: {
+            from: "bar_products",
+            localField: "servidores.items.productos.productId",
+            foreignField: "_id",
+            as: "products",
+            // as: "servidores.items.productos",
+          },
+        },
+        {
+          $lookup: {
+            from: "bar_business",
+            localField: "businessId",
+            foreignField: "_id",
+            as: "business",
+          },
+        },
+        {
+          $lookup: {
+            from: "bar_tables",
+            localField: "tableId",
+            foreignField: "_id",
+            as: "tables",
+          },
+        },
+        {
+          $lookup: {
+            from: "bar_users",
+            localField: "userId",
+            foreignField: "_id",
+            as: "users",
+          },
+        },
+      ]).toArray()
+
+      if (results.length === 0) throw new BadRequest('Products not found');
+
+      return {
+        success: true,
+        order: results
+      };
+    } catch (error) {
+      return { success: false, error };
+    }
+  }
+
+  async orderById(businessId, orderId) {
+    try {
+      const results = await db.collection('bar_orders').aggregate([
+        {
+          $match: {
+            businessId: new ObjectId(businessId),
+            _id: new ObjectId(orderId)
           },
         },
         {
