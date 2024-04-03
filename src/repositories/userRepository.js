047e25922ac08577bd4c87ad0a7e35db86c394c9
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-03 11:36:28 -0500

    Add findUserByEmail method to UserRepository

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -77,118 +77,23 @@ class UserRepository {
 
     }
 
+    async findUserByEmail(data) {
 
+        try {
 
+            const user = await this.#userModel.collection('bar_users').find({ email: data }).toArray();
+            if (user.length === 0) throw new BadRequest('El Email no existe');
 
+            return {
+                success: true,
+                user
+            }
 
+        } catch (error) {
+            return { success: false, error };
 
-
-
-    // async listOrders(businessId, userId) {
-    //     try {
-    //         const results = await this.orderModel.collection('bar_orders').aggregate([
-    //             {
-    //                 $match: {
-    //                     businessId: new ObjectId(businessId),
-    //                 },
-    //             },
-    //             {
-    //                 $lookup: {
-    //                     from: "bar_products",
-    //                     localField: "servidores.items.productos.productId",
-    //                     foreignField: "_id",
-    //                     as: "products",
-    //                     // as: "servidores.items.productos",
-    //                 },
-    //             },
-    //             {
-    //                 $lookup: {
-    //                     from: "bar_business",
-    //                     localField: "businessId",
-    //                     foreignField: "_id",
-    //                     as: "business",
-    //                 },
-    //             },
-    //             {
-    //                 $lookup: {
-    //                     from: "bar_tables",
-    //                     localField: "tableId",
-    //                     foreignField: "_id",
-    //                     as: "tables",
-    //                 },
-    //             },
-    //             {
-    //                 $lookup: {
-    //                     from: "bar_users",
-    //                     localField: "servidores.userId",
-    //                     foreignField: "_id",
-    //                     as: "users",
-    //                 },
-    //             },
-    //         ]).toArray()
-
-    //         if (results.length === 0) throw new BadRequest('Products not found');
-
-    //         return {
-    //             success: true,
-    //             order: results
-    //         };
-
-    //     } catch (error) {
-    //         return { success: false, error };
-    //     }
-    // }
-
-
-
-    // async updateOrderById(businessId, orderId, save) {
-    //     try {
-    //         const results = await this.orderModel.collection('bar_orders').updateOne(
-    //             {
-    //                 businessId: new ObjectId(businessId),
-    //                 _id: new ObjectId(orderId)
-    //             },
-    //             {
-    //                 $set: {
-    //                     ...save
-    //                 }
-    //             }
-    //         )
-
-    //         if (results.modifiedCount === 0) {
-    //             throw new BadRequest('No Ordenes para actualizar');
-    //         }
-    //         if (results.acknowledged === false) throw new BadRequest("Error al actualizar el pedido");
-
-    //         return {
-    //             success: true,
-    //             order: results
-    //         };
-    //     } catch (error) {
-    //         return { success: false, error };
-    //     }
-    // }
-
-    // async deleteOrders(businessId, orderId) {
-    //     try {
-    //         const results = await this.orderModel.collection('bar_orders').deleteOne(
-    //             {
-    //                 businessId: new ObjectId(businessId),
-    //                 _id: new ObjectId(orderId)
-    //             }
-    //         )
-
-    //         if (results.deletedCount === 0) throw new BadRequest('Products not found');
-
-    //         return {
-    //             success: true,
-    //             order: results
-    //         }
-
-    //     } catch (error) {
-    //         return { success: false, error };
-    //     }
-    // }
+        }
+    }
 }
 
 export default UserRepository;
