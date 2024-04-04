6a0dc1161a72e7e98bcfe027c4de3d6a44375912
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-04 08:58:12 -0500

    Add business and user repositories, normalize data, and update signup method

--- src/service/authService.js
+++ src/service/authService.js
@@ -11,26 +11,32 @@ import db from "../database/db.js";
 import { encryptPassword, comparePassword } from "../helper/encrypt.js";
 import { createToken } from "../helper/createToken.js";
 
+import businnessRepository from '../repositories/businessRepository.js';
+import UserRepository from '../repositories/userRepository.js';
+
+import { parserUserAuth } from '../helper/normalizeData.js';
 
 const log = log4js.getLogger(`${process.env.NODE_ENV} - normalizeData`);
 
 
-class AuthService {
+class AuthService extends businnessRepository {
     constructor() {
-        console.log("AuthService constructor");
+        super();
+
         this.BusinessServ = new BusinessService();
         this.UserServ = new UserService();
         this.roleServ = new RoleService();
+        this.businessServ = new BusinessService();
 
     }
 
     async login(data) {
-        
+
         try {
 
 
             const users = await this.UserServ.byEmailUser(data.email);
-            if (!users.success) throw new BadRequest(users.error); 
+            if (!users.success) throw new BadRequest(users.error);
 
             const { user } = users;
             const isMatch = await comparePassword(data.password, user[0].password);
@@ -38,7 +44,7 @@ class AuthService {
                 throw new BadRequest('Invalid password');
             }
             const token = await createToken(user[0]);
-            if(!token.success) throw new BadRequest(token.error);
+            if (!token.success) throw new BadRequest(token.error);
 
             const result = user[0];
             return {
@@ -53,50 +59,33 @@ class AuthService {
     }
 
     async signup(data) {
-        try {
 
             if (data.password) {
                 data.password = await encryptPassword(data.password);
             }
 
-            // const rol = await db.collection('bar_rols').insertOne({ name: 'ADMIN' })
+            const response = await this.createBusiness(data);
+            if (!response.success) throw new BadRequest(response.error);
 
-            const business = await db.collection('bar_business').insertMany([data])
+            const { business } = response;
 
-            const insertedIds = business.insertedIds;
-            const insertedData = Object.keys(insertedIds).map(key => ({
-                _id: insertedIds[key],
-                ...data
-            }));
+            const save = await parserUserAuth(data, business[0]._id)
+            if (!save.success) throw new BadRequest(save.error);
 
-            //Aactualizar el negocio con el rol
-            // await db.collection('bar_business').updateOne({ _id: insertedData[0]._id }, { $set: { rolId: rol.insertedId } })
+            const { user } = save
             
+            const results = await this.UserServ.createUsers(user);
+            if(!results.success) throw new BadRequest(results.error);
+               
+            const token = await createToken(business[0]);
+            delete business[0].password;
             
-            const user = await db.collection('bar_users').insertMany([{
-                name: insertedData[0].name,
-                lastname: insertedData[0].lastName,
-                email: insertedData[0].email,
-                password: insertedData[0].password,
-                phone: insertedData[0].phone,
-                businessId: insertedData[0]._id,
-                // rolId: rol.insertedId
-            }]) 
-            // actualizar la tabla  roles con el id del usuario
-            // await db.collection('bar_rol').updateOne({ _id: rol.insertedId }, { $set: { userId: user.insertedIds[0], businessId: business.insertedIds[0] } })        
-            
-            const token = await createToken(insertedData[0]);
-            delete insertedData[0].password;
-            // console.log('insertedData', insertedData);
-            // await this.roleServ.createPlantillas();
             return {
                 success: true,
-                data: insertedData,
+                data: business,
                 token
             };
-        } catch (error) {
-            return { success: false, error };
-        }
+        
     }
 }
 
