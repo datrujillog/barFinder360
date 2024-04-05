76bdc11fdbccf4965bf20b799ef8129c2ad2b4f2
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-04-04 23:46:47 -0500

    Corregir URL de MongoDB en config.js

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ import 'dotenv/config'
  const config = {
     jwtSecret: process.env.JW_SECRET || 'secret',
     port: process.env.PORT || 5000,
-    urlMongodb : process.env.DATABASE_URL
+    urlMongodb : process.env.DATABASE_URL || 'mongodb://mongodb:27017'
  };
 
  export default config;
